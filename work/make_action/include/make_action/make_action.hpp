c089b5d133d68d251d8cbbdc655df4eb5e862557
atrantan <atrantan@lsu.edu>
2016-02-24 16:05:18 -0600

    Changes for C++11 compliance

--- work/make_action/include/make_action/make_action.hpp
+++ work/make_action/include/make_action/make_action.hpp
@@ -4,12 +4,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ///////////////////////////////////////////////////////////////////////////////
-#if !defined(REMOTE_CALL_HPP)
-#define REMOTE_CALL_HPP
+#if !defined(MAKE_ACTION_HPP)
+#define MAKE_ACTION_HPP
 
 #include <hpx/hpx.hpp>
 #include <hpx/include/plain_actions.hpp>
-#include <tuple>
 #include <type_traits>
 
 
@@ -17,6 +16,10 @@ namespace hpx{
 // Helpers to actionize a constexpr lambda
 
     namespace detail{
+
+        template <typename ... T>
+        struct Sequence
+        {};
 
         struct addr_add
         {
@@ -28,23 +31,23 @@ namespace hpx{
         };
 
         struct caller{
-          template<typename F, typename ... Args>
-          static inline decltype(auto) call(Args && ... args)
+          template<typename F, typename ReturnType, typename ... Args>
+          static inline ReturnType call(Args && ... args)
           {
              int * dummy = nullptr;
              return reinterpret_cast<const F&>(*dummy)( std::forward<Args>(args)... );
           }
         };
 
-        template<class F, typename Sequence>
+        template<class F, typename ReturnType, typename Sequence>
         struct make_action_using_sequence
         {};
 
-        template<class F, template <typename...> class T, typename ... Args>
-        struct make_action_using_sequence< F, T<Args...> >
+        template<class F, typename ReturnType, template <typename...> class T, typename ... Args>
+        struct make_action_using_sequence< F, ReturnType, T<Args...> >
         {
-          using type = typename hpx::actions::make_direct_action<decltype(&caller::call<F,Args...>)
-                                                          ,&caller::call<F,Args...>
+          using type = typename hpx::actions::make_direct_action<decltype(&caller::call<F,ReturnType,Args...>)
+                                                          ,&caller::call<F,ReturnType,Args...>
                                                           >::type;
         };
 
@@ -56,20 +59,31 @@ namespace hpx{
         template <typename ClassType, typename ReturnType, typename... Args>
         struct extract_parameters<ReturnType(ClassType::*)(Args...) const>
         {
-            using type = std::tuple<Args...>;
+            using type = hpx::detail::Sequence<Args...>;
+        };
+
+        template <typename T>
+        struct extract_return_type
+        {};
+
+        template <typename ClassType, typename ReturnType, typename... Args>
+        struct extract_return_type<ReturnType(ClassType::*)(Args...) const>
+        {
+            using type = ReturnType;
         };
 
         template <typename F>
         struct action_from_lambda
         {
           using sequence = typename extract_parameters<decltype(&F::operator())>::type;
-          using type = typename make_action_using_sequence<F,sequence>::type;
+          using return_type = typename extract_return_type<decltype(&F::operator())>::type;
+          using type = typename make_action_using_sequence<F,return_type,sequence>::type;
         };
 
         struct action_maker
         {
             template<class F>
-            constexpr auto operator += (F* f)
+            constexpr typename action_from_lambda<F>::type operator += (F* f)
             {
                 static_assert( !std::is_assignable<F,F>() && std::is_empty<F>()
                              ,"Lambda is required"
@@ -81,7 +95,7 @@ namespace hpx{
     }
 
     template< typename F>
-    constexpr auto make_action(F && f)
+    constexpr auto make_action(F && f) -> decltype( hpx::detail::action_maker() += true ? nullptr : hpx::detail::addr_add() + f )
     {
       return hpx::detail::action_maker() += true ? nullptr : hpx::detail::addr_add() + f;
     }
