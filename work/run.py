08911fa6cc3af97d75626440e8a7a9ca7978a639
atrantan <atrantan@lsu.edu>
2016-03-20 23:31:56 -0500

    Update benchmark scripts

--- work/run.py
+++ work/run.py
@@ -2,6 +2,9 @@
 
 import os
 import re
+import sys
+
+path = sys.argv[1]
 
 CSI="\x1B["
 reset=CSI+"m"
@@ -24,14 +27,14 @@ def numaline(n):
 		return "numactl -i 0,1,6,7,2,3 "
 
 #parses a file fi
-def parse_file(f, value_name):
+def parse_file(f, value_name, unit):
 	content = f.read()
 	items=re.findall("{0}.*$".format(value_name), content, re.MULTILINE)
 	#print content
 	value_s = '0'
 	for item in items:
 		value_s = item.partition(':')[2]
-		value_s = value_s.partition('GB/s')[0]
+		value_s = value_s.partition(unit)[0]
 	return float(value_s)
 
 def valid_line(f, col):
@@ -42,7 +45,7 @@ def valid_line(f, col):
 	return True;
 
 #function to plot scalability graphs
-def plot_scalability(apps, title,  xlabel, xlabel_range, ylabel, ylabel_range, zlabel, zlabel_range, dump_file_format):
+def plot_scalability(apps, title,  xlabel, xlabel_range, ylabel, ylabel_range, zlabel, zlabel_range, dump_file_format, unit):
 	for app in apps:
 		for i in range(0, len(ylabel_range)):
 			datafile_n = "perfs/{0}/{0}_{1}={2}_{3}={4}_scale_over_{5}.dat".format(app, ylabel, ylabel_range[i], zlabel, zlabel_range[i], xlabel)
@@ -65,11 +68,36 @@ def plot_scalability(apps, title,  xlabel, xlabel_range, ylabel, ylabel_range, z
 				datafile.write("\n")
 	        datafile.close();
 
+def plot_scalability(apps, title,  xlabel, xlabel_range, ylabel, ylabel_range, dump_file_format, unit):
+	for app in apps:
+		for i in range(0, len(ylabel_range)):
+			datafile_n = "perfs/{0}/{0}_{1}={2}_scale_over_{3}.dat".format(app, ylabel, ylabel_range[i], xlabel)
+			datafile = open(datafile_n, 'w')
+			for x in xlabel_range:
+				datafile.write("{0}".format(x))
+				value_v = [];
+				total_time = 0;
+				try:
+					dumpfile_n = dump_file_format.replace("#0", x)
+					dumpfile_n = dumpfile_n.replace("#1", ylabel_range[i])
+					dumpfile_n = "perfs/{0}/{0}_{1}.dump".format(app, dumpfile_n)
+					#get value
+					dumpfile = open(dumpfile_n)
+					total_time = parse_file(dumpfile, "performances", unit)
+				except IOError:
+					total_time = 0
+				datafile.write(" {0}".format(total_time))
+				datafile.write("\n")
+	        datafile.close();
+
+
+problem = ["s3dkt3m2","memplus"]
 ncores = ['1', '2', '4', '6', '8', '10','12','14', '16']
+
 matrix_order = ['10000', '14000', '18000', '22000']
 partition_order = ['500', '700', '900', '1100']
 
-#create mpi dump directory
+#create hpx dump directory
 dumpdir = 'perfs/pvector_view_transpose'
 if not os.path.exists(dumpdir):
  	print "creating "+dumpdir+" directory"
@@ -108,9 +136,36 @@ for n in ncores:
 		print cmdline
 		os.system(cmdline)
 
-plot_scalability(["pvector_view_transpose","pvector_view_transpose_with_tasks"], "Scalability of transpose with co-array"
+plot_scalability(["pvector_view_transpose","pvector_view_transpose_with_tasks","intel_openmp_transpose"], "Scalability of transpose with co-array"
 	            , "ncores", ncores
-				, "matrix_order", matrix_order
-				, "partition_order", partition_order
-				, "matrix_order=#1_partition_order=#2_ncores=#0"
+							, "matrix_order", matrix_order
+							, "partition_order", partition_order
+							, "matrix_order=#1_partition_order=#2_ncores=#0"
+              , 'GB/s'
 				)
+
+#create hpx dump directory
+dumpdir = 'perfs/pvector_view_spmv'
+if not os.path.exists(dumpdir):
+	print "creating "+dumpdir+" directory"
+	os.makedirs(dumpdir)
+
+#hpx actions version
+for n in ncores:
+	for i in range(0, len(problem)):
+		dumpfile = dumpdir+"/pvector_view_spmv_problem={0}_ncores={1}.dump".format(problem[i], n)
+
+		cmdline = "mpirun -np "+n+" ./bin/pvector_view_spmv_test -t 1 --filename {0}/pvector_view/tests/performance/spmv/ExampleMatrices/{1}.mtx > {2}".format(
+		  path
+		, problem[i]
+		, dumpfile
+		)
+		print cmdline
+		os.system(cmdline)
+
+plot_scalability(["pvector_view_spmv"], "Scalability of spmv with co-array"
+		            , "ncores", ncores
+								, "problem", problem
+								, "problem=#1_ncores=#0"
+								, 'GFlops'
+								)
